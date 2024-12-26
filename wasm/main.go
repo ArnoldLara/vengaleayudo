2e199f54de927932d0b62a722a441d8fb04fa469
alarav <lara.arnoldandres@gmail.com>
2024-12-26 09:46:29 -0500

    adding some styles to webassembly page

--- wasm/main.go
+++ wasm/main.go
@@ -3,6 +3,5 @@ package main
 import "fmt"
 
 func main() {
- fmt.Println("This message was printed by Golang running in your browser with WebAssembly!")
- fmt.Println(6 + 2)
+ fmt.Println("This message was printed by a WebAssembly executable created from a Golang script!")
 }
